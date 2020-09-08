a83bf28bbb6ea3c30712feb5cd2156d3a4c14646
huangzx <2511646043@qq.com>
2020-09-08 16:49:34 +0800

    add echarts-demo

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <EchartDemo/> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// import EchartDemo from './components/demo/EchartDemo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    // EchartDemo
   }
 }
 </script>
