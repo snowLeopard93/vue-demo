658eab9323755edbd94686b39c266ff648c80736
huangzx <2511646043@qq.com>
2020-09-23 11:18:15 +0800

    add echart-demo

---
+++ vue-cli/vue-echart-demo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <EchartDemo/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import EchartDemo from './components/EchartDemo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    EchartDemo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
